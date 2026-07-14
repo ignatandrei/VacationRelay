1bb52a05449c606d999aabeb7485a9275dea6042
Andrei Ignat <ignatandrei@yahoo.com>
2026-07-14 10:23:21 +0300

    adding test ui

---
+++ src/VacationRelay/VR.TestUI/tests/seed.spec.ts
@@ -0,0 +1,8 @@
+import { test, expect } from '@playwright/test';
+
+test.describe('Test group', () => {
+  test('seed', async ({ page }) => {
+    // generate code here.
+    await page.goto('https://vacationrelay.dev.localhost:17071/login?t=e1dfab69a928830bb11c9a4af2a17219');
+  });
+});
